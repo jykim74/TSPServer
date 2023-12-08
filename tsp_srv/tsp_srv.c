db30c15ad90069911e64c5ff2c1cd90c3ea163dc
jykim <jykim@ranix.co.kr>
2023-12-08 15:21:07 +0900

    Process 서버 지원

--- tsp_srv/tsp_srv.c
+++ tsp_srv/tsp_srv.c
@@ -619,6 +619,28 @@ void printUsage()
     printf( "-h         : Print this message\n" );
 }
 
+#if !defined WIN32 && defined USE_PRC
+static int MainProcessInit()
+{
+    return 0;
+}
+
+static int MainProcessTerm()
+{
+    return 0;
+}
+
+static int ChildProcessInit()
+{
+    return 0;
+}
+
+static int ChildProcessTerm()
+{
+    return 0;
+}
+#endif
+
 int main( int argc, char *argv[] )
 {
     int ret = 0;
@@ -701,11 +723,31 @@ int main( int argc, char *argv[] )
         if( db ) JS_DB_close( db );
     }
 
+#if !defined WIN32 && defined USE_PRC
+    JProcInit sProcInit;
+
+    memset( &sProcInit, 0x00, sizeof(JProcInit));
+
+    sProcInit.nCreateNum = 1;
+    sProcInit.ParentInitFunction = MainProcessInit;
+    sProcInit.ParemtTermFunction = MainProcessTerm;
+    sProcInit.ChidInitFunction = ChildProcessInit;
+    sProcInit.ChildTermFunction = ChildProcessTerm;
+
+    JS_PRC_initRegister( &sProcInit );
+    JS_PRC_register( "JS_TSP", NULL, g_nPort, 4, TSP_Service );
+    JS_PRC_register( "JS_TSP_SSL", NULL, g_nSSLPort, 4, TSP_SSL_Service );
+    JS_PRC_registerAdmin( NULL, g_nPort + 10 );
+
+    JS_PRC_start();
+    JS_PRC_detach();
+#else
     JS_THD_logInit( "./log", "tsp", 2 );
     JS_THD_registerService( "JS_TSP", NULL, g_nPort, 4, TSP_Service );
     JS_THD_registerService( "JS_TSP_SSL", NULL, g_nSSLPort, 4, TSP_SSL_Service );
     JS_THD_registerAdmin( NULL, g_nPort + 10 );
     JS_THD_serviceStartAll();
+#endif
 
     return 0;
 }
